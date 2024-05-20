f09bd9b1d6a2c7331ec956378585b3dadc2d640c
gri0005 <s4002908@student.rmit.edu.au>
2024-05-20 13:09:20 +1000

    finishing touches?

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,3 +1,4 @@
+// declaring Everything
 const audio = document.querySelector("#custom-audio-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
@@ -7,11 +8,20 @@ const timerTimeSelector = document.querySelector("#timer-select");
 const timerInfo = document.querySelector("#timer-info");
 const countdownDisplay = document.querySelector("#time-remaining");
 const ambBirdButton = document.querySelector("#amb-bird-btn");
+const ambWavesButton = document.querySelector("#amb-waves-btn");
+const ambTreesButton = document.querySelector("#amb-trees-btn");
+const ambRainButton = document.querySelector("#amb-rain-btn");
 const birdSound = document.querySelector("#bird-sound");
+const wavesSound = document.querySelector("#waves-sound");
+const treesSound = document.querySelector("#trees-sound");
+const rainSound = document.querySelector("#rain-sound");
 let isPlaying = false;
 let intervalID;
 let timerDuration = 10;
 let birdPlaying = false;
+let wavesPlaying = false;
+let treesPlaying = false;
+let rainPlaying = false;
 
 // displays countdown length
 timerTimeSelector.addEventListener("change", setTimerText);
@@ -21,10 +31,71 @@ function setTimerText() {
   timerDuration = timerTimeSelector.value * 60;
 }
 
+// listens for clicks
 playPauseBtn.addEventListener("click", togglePlayPause);
 ambBirdButton.addEventListener("click", toggleBird);
+ambWavesButton.addEventListener("click", toggleWaves);
+ambTreesButton.addEventListener("click", toggleTrees);
+ambRainButton.addEventListener("click", toggleRain);
 
-// toggles bird amb sounds on and off, only plays while the timer is going
+// toggles rain amb button styling
+// if the main audio is playing, plays rain amb sounds
+// toggles "rainPlaying" variable
+// specifies volume
+function toggleRain() {
+  if (!rainPlaying) {
+    ambRainButton.classList.remove("ambs");
+    ambRainButton.classList.add("clicked-ambs");
+    rainPlaying = true;
+    if (isPlaying) {
+      rainSound.play();
+    }
+  } else {
+    ambRainButton.classList.remove("clicked-ambs");
+    ambRainButton.classList.add("ambs");
+    rainSound.pause();
+    rainPlaying = false;
+  }
+  rainSound.volume = 0.08;
+}
+
+// toggles trees amb same as above
+function toggleTrees() {
+  if (!treesPlaying) {
+    ambTreesButton.classList.remove("ambs");
+    ambTreesButton.classList.add("clicked-ambs");
+    treesPlaying = true;
+    if (isPlaying) {
+      treesSound.play();
+    }
+  } else {
+    ambTreesButton.classList.remove("clicked-ambs");
+    ambTreesButton.classList.add("ambs");
+    treesSound.pause();
+    treesPlaying = false;
+  }
+  treesSound.volume = 1;
+}
+
+// toggles waves amb
+function toggleWaves() {
+  if (!wavesPlaying) {
+    ambWavesButton.classList.remove("ambs");
+    ambWavesButton.classList.add("clicked-ambs");
+    wavesPlaying = true;
+    if (isPlaying) {
+      wavesSound.play();
+    }
+  } else {
+    ambWavesButton.classList.remove("clicked-ambs");
+    ambWavesButton.classList.add("ambs");
+    wavesSound.pause();
+    wavesPlaying = false;
+  }
+  wavesSound.volume = 0.07;
+}
+
+// toggles bird amb
 function toggleBird() {
   if (!birdPlaying) {
     ambBirdButton.classList.remove("ambs");
@@ -39,11 +110,15 @@ function toggleBird() {
     birdSound.pause();
     birdPlaying = false;
   }
+  birdPlaying.volume = 1;
 }
 
 // triggers everything when play/stop button pressed
 function togglePlayPause() {
   let intervalID;
+
+  // triggers timer, plays audio, switches icon, plays any
+  // ambiances that have their boolean set to true/have been toggled on
   if (!isPlaying) {
     audio.play();
     playPauseImg.src = "icons8-stop-90.png";
@@ -53,9 +128,24 @@ function togglePlayPause() {
     if (birdPlaying) {
       birdSound.play();
     }
+    if (wavesPlaying) {
+      wavesSound.play();
+    }
+    if (treesPlaying) {
+      treesSound.play();
+    }
+    if (rainPlaying) {
+      rainSound.play();
+    }
+
+    // triggers timer reset, switches icon,
+    // pauses all audio, resets main audio to the start
   } else {
     audio.pause();
     birdSound.pause();
+    wavesSound.pause();
+    treesSound.pause();
+    rainSound.pause();
     audio.currentTime = 0;
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
     isPlaying = false;
